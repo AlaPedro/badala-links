82a10337dc881291f10d91915416161dca5cab82
AlaPedro <alanpedro94@gmail.com>
2023-07-27 14:42:59 -0300

    Badala links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            gradientColors: {
+                "gradient-radial-light":
+                    "bg-gradient-to-tr from-pink-300 via-transparent to-pink-300",
+                "gradient-radial-dark":
+                    "bg-gradient-to-tr from-pink-900 via-black to-pink-900",
+            },
+            colors: {
+                rose: "#ffb8ca",
+                rosedark: "#ff6088",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
